54e68d2dc030e30a9c3d14fa095bd53c50b98ebc
yenilikci <m.melihcelik017@outlook.com>
2021-08-05 00:39:13 +0300

    Add routing and navigation

---
+++ reactjs-dotnetcore-mongodb/client/src/components/Employee.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Employee extends Component {
+    render() {
+        return (
+            <div>
+                <h3>This is employee page</h3>
+            </div>
+        )
+    }
+}
