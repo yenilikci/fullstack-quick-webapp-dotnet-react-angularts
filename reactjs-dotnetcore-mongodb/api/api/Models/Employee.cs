cc5c759ae0442c3082155ec59e542efc6a77cfa2
yenilikci <m.melihcelik017@outlook.com>
2021-08-04 23:14:45 +0300

    Add employee screen APIs

--- reactjs-dotnetcore-mongodb/api/api/Models/Employee.cs
+++ reactjs-dotnetcore-mongodb/api/api/Models/Employee.cs
@@ -13,6 +13,7 @@ namespace api.Models
         public int EmployeeId { get; set; }
         public string EmployeeName { get; set; }
         public string Department { get; set; }
+        public string DateOfJoining { get; set; }
         public string PhotoFileName { get; set; }
 
     }
