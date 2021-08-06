069f9a17293445aa01ca6d72afdc51ef5e01ead7
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-07 01:07:29 +0300

    Add upload file api

--- reactjs-dotnetcore-mysql/api/api/Startup.cs
+++ reactjs-dotnetcore-mysql/api/api/Startup.cs
@@ -10,6 +10,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Serialization;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace api
 {
@@ -59,6 +61,13 @@ namespace api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                     Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
+                RequestPath = "/Photos"
             });
         }
     }
