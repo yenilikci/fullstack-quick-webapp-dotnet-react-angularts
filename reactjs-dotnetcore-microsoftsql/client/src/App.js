670d57d41672b9d6acfbdf53038853e8e2395ede
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-06 17:11:01 +0300

    Create react.js project

---
+++ reactjs-dotnetcore-microsoftsql/client/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <h3 className="d-flex justify-content-center m-3">React JS Frontend</h3>
+    </div>
+  );
+}
+
+export default App;
