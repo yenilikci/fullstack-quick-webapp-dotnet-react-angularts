9f25adccacf1e5d8dd463d44d374bb85b82c77b9
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-07 02:51:15 +0300

    Add routing and navigation

---
+++ reactjs-dotnetcore-mysql/client/src/components/Department.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Department extends Component {
+    render() {
+        return (
+            <div>
+                <h3>This is department page</h3>
+            </div>
+        )
+    }
+} 
