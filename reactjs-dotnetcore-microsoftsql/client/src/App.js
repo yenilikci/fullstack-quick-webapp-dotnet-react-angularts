e472e90482b6dbf0786929d0504f3f5929e73fd6
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-06 17:18:43 +0300

    Add routing and navigation

--- reactjs-dotnetcore-microsoftsql/client/src/App.js
+++ reactjs-dotnetcore-microsoftsql/client/src/App.js
@@ -1,11 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Home } from "./components/Home";
+import { Department } from "./components/Department";
+import { Employee } from "./components/Employee";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App container">
-      <h3 className="d-flex justify-content-center m-3">React JS Frontend</h3>
-    </div>
+    <BrowserRouter>
+      <div className="App container">
+        <h3 className="d-flex justify-content-center m-3">React JS Frontend</h3>
+
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark my-3">
+          <ul className="navbar-nav">
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink
+                className="btn btn-light btn-outline-primary"
+                to="/department"
+              >
+                Department
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink
+                className="btn btn-light btn-outline-primary"
+                to="/employee"
+              >
+                Employee
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/department" component={Department} />
+          <Route path="/employee" component={Employee} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ reactjs-dotnetcore-microsoftsql/client/src/components/Employee.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class Employee extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is Employee page</h3>
+      </div>
+    );
+  }
+}
