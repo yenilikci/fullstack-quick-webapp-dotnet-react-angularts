bb487c818692038e0537f97aaa3bcd990ecfef87
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-08 00:22:00 +0300

    Add modal popup window and crud operations

--- angular-dotnetcore-microsoftsql/client/src/app/department/department.component.ts
+++ angular-dotnetcore-microsoftsql/client/src/app/department/department.component.ts
@@ -11,6 +11,9 @@ export class DepartmentComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   departments: any = [];
+  modalTitle = '';
+  DepartmentId = 0;
+  DepartmentName = '';
 
   ngOnInit(): void {
     this.refreshList();
@@ -20,5 +23,51 @@ export class DepartmentComponent implements OnInit {
     this.http.get<any>(environment.API_URL + 'department').subscribe((data) => {
       this.departments = data;
     });
+  }
+
+  addClick() {
+    this.modalTitle = 'Add Department';
+    this.DepartmentId = 0;
+    this.DepartmentName = '';
+  }
+
+  editClick(dep: any) {
+    this.modalTitle = 'Edit Department';
+    this.DepartmentId = dep.DepartmentId;
+    this.DepartmentName = dep.DepartmentName;
+  }
+
+  createClick() {
+    var val = {
+      DepartmentName: this.DepartmentName,
+    };
+
+    this.http.post(environment.API_URL + 'department', val).subscribe((res) => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  updateClick() {
+    var val = {
+      DepartmentId: this.DepartmentId,
+      DepartmentName: this.DepartmentName,
+    };
+
+    this.http.put(environment.API_URL + 'department', val).subscribe((res) => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  deleteClick(id: any) {
+    if (confirm('Are you sure?')) {
+      this.http
+        .delete(environment.API_URL + 'department/' + id)
+        .subscribe((res) => {
+          alert(res.toString());
+          this.refreshList();
+        });
+    }
   }
 }
