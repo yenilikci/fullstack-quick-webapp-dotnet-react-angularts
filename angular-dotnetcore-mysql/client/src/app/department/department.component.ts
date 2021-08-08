1dca8d7d059e1ff43021b40e6a1382d30a12fba9
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-09 00:28:43 +0300

    Add bootstrap table to department component

--- angular-dotnetcore-mysql/client/src/app/department/department.component.ts
+++ angular-dotnetcore-mysql/client/src/app/department/department.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
-  styleUrls: ['./department.component.css']
+  styleUrls: ['./department.component.css'],
 })
 export class DepartmentComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  departments: any = [];
 
   ngOnInit(): void {
+    this.refreshList();
   }
 
+  refreshList() {
+    this.http.get<any>(environment.API_URL + 'department').subscribe((data) => {
+      this.departments = data;
+    });
+  }
 }
