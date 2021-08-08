b6e7293857a35d3cca52ccc4c13df0aef15989cf
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-09 02:02:59 +0300

    Create models

---
+++ angular-dotnetcore-postgresql/api/api/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    [Table("department", Schema = "public")]
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ angular-dotnetcore-postgresql/api/api/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    [Table("employee", Schema = "public")]
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
