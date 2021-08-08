c88dc85adc60e2d66fee5d9e4979a5cf020403c8
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-08 17:10:03 +0300

    Add mongodb driver and create models

---
+++ angular-dotnetcore-mongodb/api/api/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Department
+    {
+        public ObjectId Id { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
