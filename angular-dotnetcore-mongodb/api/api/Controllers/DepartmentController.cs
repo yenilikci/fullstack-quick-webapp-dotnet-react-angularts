05a6d7c399d0e0bf23ca26ad664b8366746ad455
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-08 17:15:19 +0300

    Add department screen APIs

---
+++ angular-dotnetcore-mongodb/api/api/Controllers/DepartmentController.cs
@@ -0,0 +1,63 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var dbList = dbClient.GetDatabase("testdatabase").GetCollection<Department>("Department").AsQueryable();
+            return new JsonResult(dbList);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            int LastDepartmentId = dbClient.GetDatabase("testdatabase").GetCollection<Department>("Department").AsQueryable().Count();
+            dep.DepartmentId = LastDepartmentId + 1;
+
+            dbClient.GetDatabase("testdatabase").GetCollection<Department>("Department").InsertOne(dep);
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", dep.DepartmentId);
+            var update = Builders<Department>.Update.Set("DepartmentName", dep.DepartmentName);
+
+            dbClient.GetDatabase("testdatabase").GetCollection<Department>("Department").UpdateOne(filter, update);
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", id);
+
+            dbClient.GetDatabase("testdatabase").GetCollection<Department>("Department").DeleteOne(filter);
+            return new JsonResult("Deleted Successfully");
+        }
+
+    }
+}
