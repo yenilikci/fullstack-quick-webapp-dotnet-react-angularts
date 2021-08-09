5d63c1a9b649e7cdb7580344568a3a243b99a68e
yenilikci <contact@muhammedmelihcelik.com.tr>
2021-08-09 03:06:08 +0300

    Add modal popup window and crud operations

--- angular-dotnetcore-postgresql/client/src/app/department/department.component.ts
+++ angular-dotnetcore-postgresql/client/src/app/department/department.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
@@ -12,6 +11,9 @@ export class DepartmentComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   departments: any = [];
+  modalTitle = '';
+  DepartmentId = 0;
+  DepartmentName = '';
 
   ngOnInit(): void {
     this.refreshList();
@@ -21,5 +23,51 @@ export class DepartmentComponent implements OnInit {
     this.http.get<any>(environment.API_URL + 'department').subscribe((data) => {
       this.departments = data;
     });
+  }
+
+  addClick() {
+    this.modalTitle = 'Add Department';
+    this.DepartmentId = 0;
+    this.DepartmentName = '';
+  }
+
+  editClick(dep: any) {
+    this.modalTitle = 'Edit Department';
+    this.DepartmentId = dep.DepartmentId;
+    this.DepartmentName = dep.DepartmentName;
+  }
+
+  createClick() {
+    var val = {
+      DepartmentName: this.DepartmentName,
+    };
+
+    this.http.post(environment.API_URL + 'department', val).subscribe((res) => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  updateClick() {
+    var val = {
+      DepartmentId: this.DepartmentId,
+      DepartmentName: this.DepartmentName,
+    };
+
+    this.http.put(environment.API_URL + 'department', val).subscribe((res) => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  deleteClick(id: any) {
+    if (confirm('Are you sure?')) {
+      this.http
+        .delete(environment.API_URL + 'department/' + id)
+        .subscribe((res) => {
+          alert(res.toString());
+          this.refreshList();
+        });
+    }
   }
 }
